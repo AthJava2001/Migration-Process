c3053838d4f8d9f3bcde6eb1861015d17e074bb7
AthJava2001 <126794384+AthJava2001@users.noreply.github.com>
2023-03-03 23:49:50 +0530

    Update NodeAssert.js

--- Distributed System/NodeAssert.js
+++ Distributed System/NodeAssert.js
@@ -2,8 +2,8 @@ const assert=require('assert');
 let x=5;
 let y=6;
 try{
-  assert(x>y);
-  console.log("X is greater than y");
+  assert(x==y);
+  console.log("X is equal to y");
 }catch(error){
   console.log("Error is ",error);
 } 
